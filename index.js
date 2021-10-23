6ee6e3f5f1399398552f709ca4beb9b3b96df473
antoine <antoine.gayoso@gmail.com>
2021-10-23 13:01:36 +0200

    1 solution

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = anagrams = (stringA, stringB) => 
+    cleanStr(stringA) === cleanStr(stringB);
+
+const cleanStr = (str) => {
+    return str
+        .replace(/[\W]/g, '')
+        .split("")
+        .sort()
+        .join("")
+}
